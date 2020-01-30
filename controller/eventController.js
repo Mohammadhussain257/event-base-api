bfe10692e3df468fb21530aba73ec662760b9d15
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-01-30 22:35:07 +0545

    add vcs and readme file

--- controller/eventController.js
+++ controller/eventController.js
@@ -1,16 +1,16 @@
-/* project API routes */
+/* Events API routes */
 
 const express = require('express'),
     eventController = express.Router(),
     url = require('url');
 
-let Events = require('../models/event');
+let Events = require('../model/event');
 
 // add new event
 eventController.route('/add').post(function (req, res) {
     let event = new Events(req.body);
     event.save()
-        .then(project => {
+        .then(event => {
             res.status(200).json({ 'Success': true })
         }).catch(err => {
             res.status(400).send({ 'Success': false, 'Message': 'Error occured while creating new event' });
