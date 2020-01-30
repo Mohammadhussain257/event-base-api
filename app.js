9ed584718512545502ea4a273267641f664913e5
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-01-30 23:18:56 +0545

    fix object type error

--- app.js
+++ app.js
@@ -4,22 +4,24 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     mongoose = require('mongoose'),
-    DBconfig = require('./config/dbConfig');
+    DBconfig = require('./config/dbConfig'),
+    eventController  = require('./controller/eventController');
 const app = express();
-let port = process.env.PORT || 4300;
+const port = process.env.PORT || 4300;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(DBconfig.ConnectionString,{useNewUrlParser:true, useUnifiedTopology:true}).then(
+mongoose.connect(DBconfig.ConnectionString,{useNewUrlParser:true,useUnifiedTopology:true}).then(
     () => {console.log('Connected to Database') },
-    err => { console.log('Can not connect to the database'+ err)}
+    err => { console.log('Can not connect to database'+ err)}
 );
 
 mongoose.set('debug', true);
 
+app.use('/events', eventController);
 
 app.listen(port, function(){
     console.log('Listening on port ' + port);
--- controller/eventController.js
+++ controller/eventController.js
@@ -4,15 +4,16 @@ const express = require('express'),
     eventController = express.Router(),
     url = require('url');
 
-let Events = require('../model/event');
+let Event = require('../model/event');
 
 // add new event
 eventController.route('/add').post(function (req, res) {
-    let event = new Events(req.body);
-    event.save()
-        .then(event => {
-            res.status(200).json({ 'Success': true })
+    let newEvent = new Event(req.body);
+    newEvent.save().then(newEvent => {
+            res.status(200).json(newEvent)
         }).catch(err => {
-            res.status(400).send({ 'Success': false, 'Message': 'Error occured while creating new event' });
+            res.status(400).send({ 'Could not create event': err });
         });
 });
+
+module.exports = eventController;
